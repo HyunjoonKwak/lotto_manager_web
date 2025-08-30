61a1a5a54c96f587b26325106e0daaa10f321e36
HyunjoonKwak <hyunjoon.kwak@gmail.com>
2025-08-30 11:46:46 +0900

    Python3.8 호환성 유지

--- flask_starter/run.py
+++ flask_starter/run.py
@@ -3,7 +3,7 @@ from app import create_app
 
 def main() -> None:
     app = create_app()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
 
 
 if __name__ == "__main__":
